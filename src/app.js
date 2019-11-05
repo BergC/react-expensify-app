d017f36bafa66cec51f0b25d943a28d619e232ec
Chris <christophertberg@gmail.com>
2019-11-04 22:46:38 -0800

    Refactored code to begin integrating REDUX with our REACT code.

--- src/app.js
+++ src/app.js
@@ -5,10 +5,36 @@ import ReactDOM from 'react-dom';
 // Import app.
 import AppRouter from './routers/AppRouter';
 
+// Import store.
+import configureStore from './store/configureStore';
+
+// Import actions.
+import { addExpense } from './actions/expenses';
+
+// Import filters.
+import { setTextFilter } from './actions/filters';
+
+// Import selectors.
+import getVisibleExpenses from './selectors/expenses';
+
 // Normalize.css import for CSS reset to all browsers.
 import 'normalize.css/normalize.css';
 
 // Import our styles file.
 import './styles/styles.scss';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
+});
+
+store.dispatch(addExpense({ description: 'Water Bill', amount: 3450, createdAt: 11500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', amount: 3450, createdAt: 11500 }));
+store.dispatch(setTextFilter('water'));
+
+
 
 ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/reducers/expenses.js
@@ -0,0 +1,31 @@
+// Expenses Reducer
+const expensesReducerDefaultState = []
+
+export default (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+
+        case 'REMOVE_EXPENSE':
+            // Because each expense is an object we can destructure the id off of it.
+            return state.filter(({ id }) => id != action.id);
+
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    return {
+                        ...expense, 
+                        ...action.updates
+                    }
+                } else {
+                    return expense;
+                }
+            });
+
+        default:
+            return state;
+    }
+};
