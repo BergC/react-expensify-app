7659349487f6ba0b248c3cfc1d41b9f38fb392bb
Chris <christophertberg@gmail.com>
2019-11-05 20:19:31 -0800

    Began connecting REACT components with our REDUX store using react-redux and Higher Order Components.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // Import dependencies.
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 // Import app.
 import AppRouter from './routers/AppRouter';
@@ -25,16 +26,22 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
 store.dispatch(addExpense({ description: 'Water Bill', amount: 3450, createdAt: 11500 }));
 store.dispatch(addExpense({ description: 'Gas Bill', amount: 3450, createdAt: 11500 }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+    store.dispatch(setTextFilter('rent'));
+}, 3000);
 
+const state = store.getState();
+const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app'));
+const jsx = (
+    <Provider store={store}>
+        <AppRouter />
+    </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'));
