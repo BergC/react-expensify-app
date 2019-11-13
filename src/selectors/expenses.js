eb468aac37a154c7e9257785de5054871a439755
Chris <christophertberg@gmail.com>
2019-11-12 22:00:07 -0800

    Implemented ability for users to filter existing expenses by a date range.

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,8 +1,16 @@
+import moment from 'moment';
+
 // Get visible expenses.
 export default (expenses, { text, sortBy, startDate, endDate }) => {
     return expenses.filter((expense) => {
-        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
-        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const createdAtMoment = moment(expense.createdAt);
+
+        // Checks if start date is the same or before the created date to determine if expense should be showed.
+        const startDateMatch = startDate ? startDate.isSameOrBefore(createdAtMoment, 'day') : true ;
+
+        // Checks if end date is the same or later than the created date to determine if expense should be showed.
+        const endDateMatch = endDate ? endDate.isSameOrAfter(createdAtMoment, 'day') : true;
+
         const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
 
         return startDateMatch && endDateMatch && textMatch;
