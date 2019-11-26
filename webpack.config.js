a959f433e877e6fc736bcea6ef064b0aee8d3e7d
Chris <christophertberg@gmail.com>
2019-11-25 17:39:14 -0800

    Updated babel-loader to 8.0.0-beta6 so that we could use @babel/core with webpack dev server.  Worked through the AddExpensePage and EditExpensePage component tests, which required refactoring both components from stateless functional to class based components.

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     module: {
         rules: [{
-            loader: 'babel-loader',
+            use: 'babel-loader',
             test: /\.js$/,
             exclude: /node_modules/
         }, {
