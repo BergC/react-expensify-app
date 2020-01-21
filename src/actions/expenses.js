e2ce660ca92985e47bcba8165bf482c9b04906be
Chris <christophertberg@gmail.com>
2020-01-20 23:04:14 -0800

    Connected to Firebase database. Setup development and test database env.

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,16 +1,33 @@
 import uuid from 'uuid';
+import database from '../firebase/firebase';
 
 // ADD_EXPENSE
-export const addExpense = ({ description = '', note = '', amount = 0, createdAt = 0 } = {}) => ({
+export const addExpense = (expense) => ({
     type: 'ADD_EXPENSE',
-    expense: {
-        id: uuid(),
-        description,
-        note,
-        amount,
-        createdAt
-    }
+    expense
 });
+
+// Start the process of dispatching addExpense
+export const startAddExpense = (expenseData = {}) => {
+    // Retruning a function in REDUX only works cause of redux-thunk middleware in configureStore.
+    return (dispatch) => {
+        const {
+            description = '', 
+            note = '', 
+            amount = 0, 
+            createdAt = 0
+        } = expenseData;
+
+        const expense = { description, note, amount, createdAt };
+
+        return database.ref('expenses').push(expense).then((ref) => {
+            dispatch(addExpense({
+                id: ref.key,
+                ...expense
+            }));
+        });
+    };
+};
 
 // REMOVE_EXPENSE
 export const removeExpense = ({ id } = {}) => ({
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,7 +1,11 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import DotEnv from 'dotenv';
 
 // Configure our adapter.
 Enzyme.configure({
     adapter: new Adapter()
 });
+
+// Configure test environment variables.
+DotEnv.config({ path: '.env.test' });
