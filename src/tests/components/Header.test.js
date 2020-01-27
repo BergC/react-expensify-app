ee9e7695cb9936dba526b65463b01149383cc342
Chris <christophertberg@gmail.com>
2020-01-26 19:51:24 -0800

    Setup test cases for the new startLogin and startLogout async processes.

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -6,3 +6,12 @@ test('Should render Header correctly', () => {
     const wrapper = shallow(<Header startLogout={() => {}} />);
     expect(wrapper).toMatchSnapshot();
 });
+
+test('Should call startLogout on button click', () => {
+    const startLogoutSpy = jest.fn();
+    const wrapper = shallow(<Header startLogout={startLogoutSpy} />);
+
+    wrapper.find('button').simulate('click');
+
+    expect(startLogoutSpy).toHaveBeenCalled();
+});
